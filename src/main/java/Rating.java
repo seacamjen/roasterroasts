0e3c2c997cb6f914dd2b04412b3bf85d944d4bfe
Cameron Jensen and Stormi Hoebelheinrich <stormi@>
2017-03-29 09:15:21 -0700

    add Rating class with getter methods and tests

--- src/main/java/Rating.java
+++ src/main/java/Rating.java
@@ -1,9 +1,33 @@
 import org.sql2o.*;
 
 public class Rating {
+  private int id;
+  private int roaster_id;
+  private String rater;
+  private int rating;
+  private String comment;
 
-  public Rating() {
+  public Rating(int roaster_id, String rater, int rating, String comment) {
+    this.roaster_id = roaster_id;
+    this.rater = rater;
+    this.rating = rating;
+    this.comment = comment;
+  }
 
+  public int getRoasterId(){
+    return roaster_id;
+  }
+
+  public String getRater(){
+    return rater;
+  }
+
+  public int getRating(){
+    return rating;
+  }
+
+  public String getComment(){
+    return comment;
   }
 
 }
--- src/test/java/RatingTest.java
+++ src/test/java/RatingTest.java
@@ -12,9 +12,37 @@ public class RatingTest {
   @After
   public void tearDown() {
     try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
+      String sql = "DELETE FROM ratings *;";
       con.createQuery(sql).executeUpdate();
     }
   }
+  @Test
+  public void Rating_instantiatesCorrectly_true(){
+    Rating testRating = new Rating(1, "name", 3, "super okay coffee");
+    assertTrue(testRating instanceof Rating);
+  }
 
+  @Test
+  public void getRoasterId_instantiatesWithRoasterId_true(){
+    Rating testRating = new Rating(1, "name", 3, "super okay coffee");
+    assertEquals(1, testRating.getRoasterId());
+  }
+
+  @Test
+  public void getRater_instantiatesWithRater_true(){
+    Rating testRating = new Rating(1, "name", 3, "super okay coffee");
+    assertEquals("name", testRating.getRater());
+  }
+
+  @Test
+  public void getRating_instantiatesWithRating_true(){
+    Rating testRating = new Rating(1, "name", 3, "super okay coffee");
+    assertEquals(3, testRating.getRating());
+  }
+
+  @Test
+  public void getComment_instantiatesWithComment_true(){
+    Rating testRating = new Rating(1, "name", 3, "super okay coffee");
+    assertEquals("super okay coffee", testRating.getComment());
+  }
 }
