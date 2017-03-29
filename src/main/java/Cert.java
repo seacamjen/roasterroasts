f5007a97608e64262156e45c7cdf7c5a35330aac
Cameron Jensen and Stormi Hoebelheinrich <cameron@>
2017-03-29 14:28:56 -0700

    Add cert and hood classes.

--- src/main/java/Cert.java
+++ src/main/java/Cert.java
@@ -1,9 +1,28 @@
 import org.sql2o.*;
 
 public class Cert {
+  private int id;
+  private String name;
 
-  public Cert() {
+  public Cert(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
 
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public static List<Cert> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM certs;";
+      return con.createQuery(sql)
+        .executeAndFetch(Cert.class);
+    }
   }
 
 }
--- src/main/java/Hood.java
+++ src/main/java/Hood.java
@@ -1,9 +1,27 @@
 import org.sql2o.*;
 
 public class Hood {
+  private int id;
+  private String name;
+  public Hood(int id,  String name) {
+    this.id = id;
+    this.name = name;
+  }
 
-  public Hood() {
+  public String getName(){
+    return name;
+  }
 
+  public int getId(){
+    return id;
+  }
+
+  public static List<Hood> all() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM hoods;";
+      return con.createQuery(sql)
+        .executeAndFetch(Hood.class);
+    }
   }
 
 }
